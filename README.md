cc0344cad76e8ed5ef2b6830e12134b5b1aa30f8
IneeddHelp <158655561+IneeddHelp@users.noreply.github.com>
2024-11-04 16:57:14 -0600

    LIEF Refactor, Inspect option, csv output
    Signature added, some redundent features removed, README updated. Readability increased

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 Go to working directory of project, and run `cargo run -- /path/to/pe/file`.
 The csv and parquet file will be placed in the `result` folder.
 
-Currently only works on POSIX compliant systems (UNIX, Linux, MacOS, etc.)
+To inspect a single file or directory, use the `-inspect` or `-i` option.
 
-Windows support coming soon...
+Such as `cargo run -- -i some_file.exe other_file.exe`. some_file.exe will be
+extracted and printed to the terminal, and NOT added to the csv
+
+Currently only works on POSIX compliant systems (UNIX, Linux, MacOS, etc.)
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,18 @@
-use chrono::Local;
-use exe::pe::{VecPE, PE};
-use rayon::prelude::*;
-use std::{
-    env,
-    error::Error,
-    fs::{self, File, OpenOptions},
-    io::Write,
-    process::Command,
-    str,
-    sync::Mutex,
-};
-mod pe_collector;
-mod pe_imports;
+use argh::FromArgs;
+use std::{error::Error, fs, process::Command};
+mod csv_builder;
+mod extract;
 
-#[derive(Debug)]
-enum ArgError {
-    InvalidArgNum,
-    InvalidFileName,
-}
+#[derive(FromArgs)]
+///Flags for feature extraction
+struct AnalysisFlags {
+    ///file or dir to print extracted elements to terminal
+    #[argh(option, short = 'i')]
+    inspect: Option<String>,
 
-fn is_valid_arg(args: &[String]) -> Result<(), ArgError> {
-    if args.len() == 1 {
-        return Err(ArgError::InvalidArgNum);
-    }
-    let mut arg_iter = args.iter();
-
-    arg_iter.next(); //skipping the cwd
-
-    for arg in arg_iter {
-        if fs::metadata(arg).is_err() {
-            eprintln!("INVALID FILE/DIRECTORY NAME: {}", arg);
-            return Err(ArgError::InvalidFileName);
-        }
-    }
-
-    Ok(())
+    /// files and dirs to extract features from
+    #[argh(greedy, positional)]
+    files: Vec<String>,
 }
 
 fn unpack_dir(dir_name: &str) -> Result<Vec<String>, Box<dyn Error>> {
@@ -57,18 +35,6 @@ fn unpack_dir(dir_name: &str) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(file_names)
 }
 
-fn convert_csv(file_name: &str) -> Result<(), Box<dyn Error>> {
-    let _ = Command::new("python3")
-        .args(["scripts/csv_to_parquet.py", file_name])
-        .output()
-        .expect("Failed to convert csv to parquet. Make sure `python3` is on your PATH");
-
-    // println!("{}", str::from_utf8(out.stdout.as_slice())?);
-    // println!("{}", str::from_utf8(out.stderr.as_slice())?);
-
-    Ok(())
-}
-
 fn collect_file_paths(file_names: &[String]) -> Result<Vec<String>, Box<dyn Error>> {
     let mut files = vec![];
 
@@ -82,109 +48,52 @@ fn collect_file_paths(file_names: &[String]) -> Result<Vec<String>, Box<dyn Erro
     Ok(files)
 }
 
-fn build_csv_rows(file_names: &[String]) -> Result<(Vec<String>, Vec<String>), Box<dyn Error>> {
-    let csv_vec_64 = Mutex::new(vec![]);
-    let csv_vec_32 = Mutex::new(vec![]);
-
-    file_names.par_iter().for_each(|file| {
-        let poss_image = VecPE::from_disk_file(file);
-
-        match poss_image {
-            Ok(image) => {
-                let is_x64 = image.get_arch().unwrap() == exe::Arch::X64;
-
-                if is_x64 {
-                    let mut v = csv_vec_64.lock().unwrap();
-                    match pe_collector::parse_64(&image) {
-                        Ok(parsed) => v.push(format!("{},{}", file, parsed)),
-                        Err(e) => eprintln!("Error parsing 64-bit image for {}, skipping... Error: {:?}", file, e),
-                    }
-                } else {
-                    let mut v = csv_vec_32.lock().unwrap();
-                    match pe_collector::parse_32(&image) {
-                        Ok(parsed) => v.push(format!("{},{}", file, parsed)),
-                        Err(e) => eprintln!("Error parsing 32-bit image for {}, skipping... Error: {:?}", file, e),
-                    }
-                }
-            }
-            Err(_) => eprintln!("Error parsing {}, skipping...", file),
-        }
-    });
-    let x64_csv = csv_vec_64.lock().unwrap().clone();
-    let x32_csv = csv_vec_32.lock().unwrap().clone();
-    Ok((x64_csv, x32_csv))
+fn convert_csv_to_parquet(file_name: &str) {
+    let _ = Command::new("python3")
+        .args(["scripts/csv_to_parquet.py", file_name])
+        .output()
+        .expect("Failed to convert csv to parquet. Make sure `python3` is on your PATH");
 }
 
-fn append_or_create_csv(
-    file_name: &str,
-    data: &Vec<String>,
-    header: Option<&str>,
-) -> Result<(), Box<dyn Error>> {
-    if fs::metadata(file_name).is_err() {
-        // if file does not exist
-        let mut f =
-            File::create(file_name).expect(format!("Unable to create file {}", file_name).as_str());
-        f.write_all(header.unwrap().as_bytes())
-            .expect("Unable to write header");
-        f.write_all("\n".as_bytes())
-            .expect("Unable to wirte newline after header");
-        for dat in data {
-            f.write_all(dat.as_bytes()).expect("Unable to write Data");
-            f.write_all("\n".as_bytes())
-                .expect("Unable to write new line");
-        }
-    } else {
-        let mut f = OpenOptions::new()
-            .append(true)
-            .open(file_name)
-            .expect("Unable to write to file");
-        let mut perms = fs::metadata(file_name)?.permissions();
-        perms.set_readonly(false);
-        fs::set_permissions(file_name, perms)?;
-        for dat in data {
-            f.write_all(dat.as_bytes()).expect("Unable to append Data");
-            f.write_all("\n".as_bytes())
-                .expect("unable to append new line");
-        }
+fn inspect(file_name: &str) {
+    let file_names = collect_file_paths(&[file_name.to_owned()]).unwrap();
+    let (x64_head, x32_head) = (
+        csv_builder::get_x64_headers(),
+        csv_builder::get_x32_headers(),
+    );
+    let (x64_rows, x32_rows) = csv_builder::build_csv_rows(&file_names);
+    for r in x64_rows {
+        print_inspect(x64_head.as_str(), r.as_str());
     }
+    for r in x32_rows {
+        print_inspect(x32_head.as_str(), r.as_str());
+    }
+}
 
-    Ok(())
+fn print_inspect(header: &str, data: &str) {
+    let head_vec: Vec<&str> = header.split(",").collect();
+    let dat_vec: Vec<&str> = data.split(",").collect();
+
+    for i in 0..head_vec.len() {
+        println!("{}: {}", head_vec[i], dat_vec[i]);
+    }
+    println!();
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let _date_time = Local::now();
-    let args: Vec<String> = env::args().collect();
-    match is_valid_arg(&args) {
-        Ok(_) => (),
-        Err(s) => {
-            println!("ERROR: {:?}", s);
-            return Ok(());
-        }
+    let inspect_arg: AnalysisFlags = argh::from_env();
+    if let Some(file_name) = inspect_arg.inspect {
+        inspect(file_name.as_str());
     }
 
-    // let csv_file_name = format!("{}", date_time.format("%Y_%m_%d-%H:%M:%S"));
-    // let csv_64_bit = format!("result/{}_x64.csv", csv_file_name);
-    // let csv_32_bit = format!("result/{}_x32.csv", csv_file_name);
+    let mut args = inspect_arg.files.clone();
+    args.retain(|s| fs::metadata(s).is_ok());
 
-    //to_owend isn't necessary, just to keep it in line iwth the timestamp file names
-    let csv_64_bit = "result/sample64.csv".to_owned();
-    let csv_32_bit = "result/sample32.csv".to_owned();
+    let files = collect_file_paths(&args)?;
 
-    let files = collect_file_paths(&args[1..])?;
-    let (csv64, csv32) = build_csv_rows(&files)?;
-    append_or_create_csv(
-        csv_64_bit.as_str(),
-        &csv64,
-        Some(pe_collector::get_csv_headers_64().as_str()),
-    )?;
-    append_or_create_csv(
-        csv_32_bit.as_str(),
-        &csv32,
-        Some(pe_collector::get_csv_headers_32().as_str()),
-    )?;
-
-    convert_csv(csv_64_bit.as_str())?;
-    convert_csv(csv_32_bit.as_str())?;
+    let (x64, x32) = csv_builder::extract_to_csv(&files);
+    convert_csv_to_parquet(x64.as_str());
+    convert_csv_to_parquet(x32.as_str());
 
     Ok(())
 }
